bbaad456038189b169d318b0b1cbd007f45796d9
barkeleyrico <barkeley.rico@gmail.com>
2025-08-27 23:08:21 +0800

    edit

--- bookmark-landing-page/bookmark-landing-page/src/components/Headers.js
+++ bookmark-landing-page/bookmark-landing-page/src/components/Headers.js
@@ -1,5 +1,19 @@
+import Logo from "../assets/logo-bookmark.svg";
+import styles from "./Headers.module.css";
 const Headers = () => {
-  return <div>Hello!</div>;
+  return (
+    <nav>
+      <div className={`${styles.container} ${styles.flex}`}>
+        <img src={Logo} alt="My Logo" />
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Contact</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Headers;
